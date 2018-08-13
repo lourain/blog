50de22955297640d27754b9f218bcd6de0121703
liyiwei <491027175@qq.com>
2018-08-13 14:28:48 +0800

    markdown

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var article = require('../lib/article')
-
+var markdown = require('markdown').markdown
 router.get('/', function (req, res, next) {
-  new article().getAll(null,function (err,articles) {
-    if(err){
+  new article().getAll(null, function (err, articles) {
+    if (err) {
       throw err;
     }
-    res.render('index', { title: '这是主页', user: req.session.user,articles:articles ,success: req.flash('success').toString(), err: req.flash('err').toString()});
+    articles.forEach(article => {
+      article.content = markdown.toHTML(article.content)
+    });
+    res.render('index', { title: '这是主页', user: req.session.user, articles: articles, success: req.flash('success').toString(), err: req.flash('err').toString() });
   })
 });
 
