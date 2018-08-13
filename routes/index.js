c9174c5905cd4197539bba99a57aaf4de6766124
liyiwei <491027175@qq.com>
2018-08-13 14:51:44 +0800

    optimize

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var article = require('../lib/article')
-var markdown = require('markdown').markdown
+
 router.get('/', function (req, res, next) {
-  new article().getAll(null, function (err, articles) {
-    if (err) {
-      throw err;
-    }
-    articles.forEach(article => {
-      article.content = markdown.toHTML(article.content)
-    });
+  new article().getAll(null, function (articles) {
     res.render('index', { title: '这是主页', user: req.session.user, articles: articles, success: req.flash('success').toString(), err: req.flash('err').toString() });
   })
 });
