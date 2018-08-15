2821c8716cc06ff611d7881aa370bf0607456a8b
liyiwei <491027175@qq.com>
2018-08-15 17:32:04 +0800

    分页

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,18 @@ var router = express.Router();
 var article = require('../lib/article')
 
 router.get('/', function (req, res, next) {
-  new article().getAll(null, function (articles) {
+  var page = req.query.p ? parseInt(req.query.p) : 1;
+
+  new article().getFive(null, page, function (articles, total) {
     res.render('index', {
       title: '这是主页',
       user: req.session.user,
-      articles: articles, 
+      articles: articles,
       err: req.flash('err').toString(),
-      success:req.flash('success').toString()
+      success: req.flash('success').toString(),
+      page:page,
+      isFirstPage: page == 1 ? true : false,
+      isLastPage: articles.length + (page - 1) * 5 == total
     });
   })
 });
