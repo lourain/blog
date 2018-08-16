cb45c317dd0b27d5d9d7051fc5cae883ba709159
liyiwei <491027175@qq.com>
2018-08-16 11:02:27 +0800

    bug

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ var Comment = require('../lib/comment')
 
 router.get('/:name',check.checkNotLogin)
 router.get('/:name', function (req, res, next) {
+  var page = req.query.p?parseInt(req.query.p):1
   //检查用户是否存在
   let user_name = req.params.name
   new User().get({ name: user_name }, function (err, users) {
@@ -16,13 +17,16 @@ router.get('/:name', function (req, res, next) {
       return res.redirect('/')
     }
     //查询并返回该用户的文章
-    new Article().getAll({ name: user_name }, function (docs) {
+    new Article().getFive({ name: user_name },page,function (docs,total) {
       res.render('user', {
         user: req.session.user,
         title: user_name,
         success: req.flash('success').toString(),
         err: req.flash('err').toString(),
-        articles: docs
+        articles: docs,
+        page:page,
+        isFirstPage:page==1,
+        isLastPage: (page - 1) * 5 + docs.length == total 
       })
     })
   })
